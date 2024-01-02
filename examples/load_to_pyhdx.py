4a9e7c4eb3994be2a6e88e88046eed2a6014fb1e
Jochem Smit <jhsmit@gmail.com>
2024-01-02 13:38:24 +0100

    new pinned requirements

--- examples/load_to_pyhdx.py
+++ examples/load_to_pyhdx.py
@@ -9,6 +9,7 @@ data_pth = test_pth / "datasets"
 vault = DataVault(cache_dir=data_pth)
 ds = vault.load_dataset("20221007_1530_SecB_Krishnamurthy")
 
+# not yet in latests release
 hdxm = HDXMeasurement.from_dataset(ds)
 
 print(hdxm)
