e31c0c8f4247be4c41f03fdc21abeb758a5108b5
Jochem Smit <jhsmit@gmail.com>
2025-05-28 16:39:07 +0200

    fix imports

--- hdxms_datasets/__init__.py
+++ hdxms_datasets/__init__.py
@@ -1,26 +1,22 @@
 """Top-level package for HDXMS Datasets."""
 
 from hdxms_datasets.__version__ import __version__
-from hdxms_datasets.datasets import DataSet, DataFile, create_dataset
+from hdxms_datasets.datasets import DataSet, PeptideTableFile, create_dataset
 from hdxms_datasets.datavault import DataVault, RemoteDataVault
 from hdxms_datasets.process import (
     convert_temperature,
     convert_time,
     filter_peptides,
-    parse_data_files,
 )
-from hdxms_datasets.reader import read_dynamx
 
 __all__ = [
     "DataSet",
-    "DataFile",
+    "PeptideTableFile",
     "create_dataset",
     "DataVault",
     "RemoteDataVault",
     "convert_temperature",
     "convert_time",
     "filter_peptides",
-    "parse_data_files",
-    "read_dynamx",
     "__version__",
 ]
