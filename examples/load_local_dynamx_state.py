bc8bb45ca89b7ab9f2e8feda081fd63421cc7a3f
Jochem Smit <jhsmit@gmail.com>
2025-07-07 14:08:56 +0200

    add structure viewing methods

--- examples/load_local_dynamx_state.py
+++ examples/load_local_dynamx_state.py
@@ -1,9 +1,11 @@
 # %%
-
 from hdxms_datasets import DataVault
 from pathlib import Path
 
+from hdxms_datasets.backend import BACKEND
 from hdxms_datasets.process import merge_peptides, compute_uptake_metrics
+import narwhals as nw
+
 
 DATASET = "1665149400_SecA_Krishnamurthy"
 # DATASET = "1704204434_SecB_Krishnamurthy"
@@ -11,21 +13,21 @@ DATASET = "1665149400_SecA_Krishnamurthy"
 # %%
 test_pth = Path(__file__).parent.parent / "tests"
 data_pth = test_pth / "datasets"
-
-# Creating a DataVault without giving a cache path name uses $home/.hdxms_datasets by default
+# %%
+# DATASET = "1665149400_SecA_Krishnamurthy"
 vault = DataVault(data_pth)
 
+# %%
 # Load the dataset
 ds = vault.load_dataset(DATASET)
 
 # Print a string describing the states in the dataset
 print(ds.describe())
-
-# Get the seqeunce of the first state
+# %%
+# Get the sequence of the first state
 state = ds.get_state(0)
 sequence = state.get_sequence()
 print(sequence)
-
 
 # %%
 # Load FD control peptides as a narwhals DataFrame
@@ -48,34 +50,25 @@ df = processed.to_native()
 print(df)
 
 # do the previous two steps in one go
-
 processed = state.compute_uptake_metrics().to_polars()
 processed
 
 # %%
-
 # show the first peptide on the structure
 start, end = processed["start", "end"].row(10)
-
-custom_data = state.structure.pdbemolstar_custom_data()
-color_data = state.structure.pdbemolstar_color_peptide(start, end)
-
-from ipymolstar import PDBeMolstar
-
-view = PDBeMolstar(
-    custom_data=custom_data,
-    color_data=color_data,
-    hide_water=True,
-    hide_carbs=True,
-)
+view = state.structure.color_peptide(start, end).show()
 view
 
 # %%
-
-# load all states
-states = [ds.get_state(state) for state in ds.states]
-for state in states:
-    df = state.compute_uptake_metrics()
-    print(df.to_polars())
+# show regions of the structure that are covered by peptides
+view = state.structure.peptide_coverage(processed, color="blue").show()
+view
 
 # %%
+# show a set of non-overlapping peptides on the structure
+view = state.structure.non_overlapping_peptides(processed).show()
+view
+
+# %%
+view = state.structure.peptide_redundancy(processed).show()
+view
