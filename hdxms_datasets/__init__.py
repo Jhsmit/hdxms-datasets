44274ff61929c19c22f5e3b728cb5bf0f22224bf
Jochem Smit <jhsmit@gmail.com>
2025-09-17 14:34:35 +0200

    style: format imports

--- hdxms_datasets/__init__.py
+++ hdxms_datasets/__init__.py
@@ -1,27 +1,26 @@
 """Top-level package for HDXMS Datasets."""
 
 from hdxms_datasets.__version__ import __version__
-
-from hdxms_datasets.models import (
-    HDXDataSet,
-    State,
-    Peptides,
-    ProteinState,
-    ProteinIdentifiers,
-    Structure,
-    Publication,
-    Author,
-    DatasetMetadata,
-)
 from hdxms_datasets.database import DataBase, RemoteDataBase, load_dataset
 from hdxms_datasets.loader import load_peptides, read_csv
-from hdxms_datasets.process import (
-    merge_peptides,
-    compute_uptake_metrics,
-    apply_filters,
-    aggregate,
+from hdxms_datasets.models import (
+    Author,
+    DatasetMetadata,
+    HDXDataSet,
+    Peptides,
+    ProteinIdentifiers,
+    ProteinState,
+    Publication,
+    State,
+    Structure,
 )
-
+from hdxms_datasets.process import (
+    aggregate,
+    apply_filters,
+    compute_uptake_metrics,
+    merge_peptides,
+)
+from hdxms_datasets.utils import verify_sequence
 
 __all__ = [
     "__version__",
@@ -43,4 +42,5 @@ __all__ = [
     "compute_uptake_metrics",
     "apply_filters",
     "aggregate",
+    "verify_sequence",
 ]
