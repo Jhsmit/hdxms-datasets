6b9c7268ed271af65b0cca5e95c2a1f13fa73bc5
Jochem Smit <jhsmit@gmail.com>
2025-12-05 13:52:46 +0100

    remove dataset builder

--- dataset-builder/backend/app/__init__.py
+++
@@ -1,1 +0,0 @@
-# HDX-MS Dataset Builder Backend
--- dataset-builder/frontend/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-import './style.css'
-
-const app = createApp(App)
-
-app.use(createPinia())
-app.mount('#app')
